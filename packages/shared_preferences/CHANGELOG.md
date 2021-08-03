50c4792056eeeb9c48c2a4b7bd45301304cb46b8
Hidenori Matsubayashi <Hidenori.Matsubayashi@sony.com>
2021-08-03 14:13:04 +0900

    Add shared_preferences (#15)

---
+++ packages/shared_preferences/CHANGELOG.md
@@ -0,0 +1,2 @@
+## 1.0.0
+* Initial release to support shared_preferences on eLinux.
---
+++ packages/shared_preferences/example/elinux/flutter/main.dart
@@ -0,0 +1,12 @@
+//
+// Generated file. Do not edit.
+//
+// @dart=2.12
+
+import 'package:shared_preferences_elinux_example/main.dart' as entrypoint;
+import 'generated_plugin_registrant.dart';
+
+Future<void> main() async {
+  registerPlugins();
+  entrypoint.main();
+}
