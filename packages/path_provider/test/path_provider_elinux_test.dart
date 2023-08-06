a34cc52248d81c9a9ac1d7cc4b8bd87b90c9265e
Hidenori Matsubayashi <hidenori.matsubayashi@gmail.com>
2023-08-06 00:04:11 +0000

    path_provider: update for official packages/path_provider 2.2.0 (#68)
    Signed-off-by: Hidenori Matsubayashi <Hidenori.Matsubayashi@gmail.com>

--- packages/path_provider/test/path_provider_elinux_test.dart
+++ packages/path_provider/test/path_provider_elinux_test.dart
@@ -1,9 +1,11 @@
+// Copyright 2023 Sony Group Corporation. All rights reserved.
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 import 'package:flutter_test/flutter_test.dart';
 import 'package:path_provider_elinux/path_provider_elinux.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+import 'package:xdg_directories/xdg_directories.dart' as xdg;
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -13,19 +15,54 @@ void main() {
     expect(PathProviderPlatform.instance, isA<PathProviderELinux>());
   });
 
-  test('getTemporaryPath', () async {
-    final PathProviderPlatform plugin = PathProviderPlatform.instance;
+  test('getTemporaryPath defaults to TMPDIR', () async {
+    final PathProviderPlatform plugin = PathProviderELinux.private(
+      environment: <String, String>{'TMPDIR': '/run/user/0/tmp'},
+    );
+    expect(await plugin.getTemporaryPath(), '/run/user/0/tmp');
+  });
+
+  test('getTemporaryPath uses fallback if TMPDIR is empty', () async {
+    final PathProviderPlatform plugin = PathProviderELinux.private(
+      environment: <String, String>{'TMPDIR': ''},
+    );
+    expect(await plugin.getTemporaryPath(), '/tmp');
+  });
+
+  test('getTemporaryPath uses fallback if TMPDIR is unset', () async {
+    final PathProviderPlatform plugin = PathProviderELinux.private(
+      environment: <String, String>{},
+    );
     expect(await plugin.getTemporaryPath(), '/tmp');
   });
 
   test('getApplicationSupportPath', () async {
-    final PathProviderPlatform plugin = PathProviderPlatform.instance;
-    expect(await plugin.getApplicationSupportPath(), startsWith('/'));
+    final PathProviderPlatform plugin = PathProviderELinux.private(
+        executableName: 'path_provider_elinux_test_binary',
+        applicationId: 'com.example.Test');
+    // Note this will fail if ${xdg.dataHome.path}/path_provider_linux_test_binary exists on the local filesystem.
+    expect(await plugin.getApplicationSupportPath(),
+        '${xdg.dataHome.path}/com.example.Test');
+  });
+
+  test('getApplicationSupportPath uses executable name if no application Id',
+      () async {
+    final PathProviderPlatform plugin = PathProviderELinux.private(
+        executableName: 'path_provider_elinux_test_binary');
+    expect(await plugin.getApplicationSupportPath(),
+        '${xdg.dataHome.path}/path_provider_elinux_test_binary');
   });
 
   test('getApplicationDocumentsPath', () async {
     final PathProviderPlatform plugin = PathProviderPlatform.instance;
     expect(await plugin.getApplicationDocumentsPath(), startsWith('/'));
+  });
+
+  test('getApplicationCachePath', () async {
+    final PathProviderPlatform plugin = PathProviderELinux.private(
+        executableName: 'path_provider_elinux_test_binary');
+    expect(await plugin.getApplicationCachePath(),
+        '${xdg.cacheHome.path}/path_provider_elinux_test_binary');
   });
 
   test('getDownloadsPath', () async {
